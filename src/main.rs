39cda3ddf4c627c00abd5be4e7ee34c00f1c0497
Yash Ladha <yashladhapankajladha123@gmail.com>
2020-09-27 08:24:28 +0530

    refactor: abstracted request generation logic
    Abstracted request generation logic to github handler as it manager the
    username and token value for the user. It also manager the repository
    fetch and pulls on the repository as the entity is done by the user in
    the real world and thus it should have the data to perform those tasks.

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,67 @@ struct Pull<'a> {
 
 static CLONE_TOKEN: &'static str = env!("CLONE_TOKEN");
 
-async fn merge_pr(pull_url: &str, number: u32, username: &str) -> Result<(), reqwest::Error> {
-    let merge_url = format!("{}/{}/merge", pull_url, number);
+struct GithubHandler<'a> {
+    username: &'a str,
+    token: &'a str,
+}
 
-    Client::new()
-        .put(merge_url.as_str())
-        .header("Accept", "application/vnd.github.v3+json")
-        .header("User-Agent", username)
-        .basic_auth(username, Some(CLONE_TOKEN.clone()))
-        .send()
-        .await?;
+impl GithubHandler<'_> {
+    fn generate_repo_list(&self, page_no: u32) -> reqwest::RequestBuilder {
+        let repo_url = format!("https://api.github.com/users/{}/repos", self.username);
+        Client::new()
+            .get(&repo_url)
+            .header("Accept", "application/vnd.github.v3+json")
+            .header("User-Agent", self.username)
+            .query(&[
+                ("sort", "updated"),
+                ("direction", "desc"),
+                ("per_page", "100"),
+                ("page", page_no.to_string().as_str()),
+            ])
+            .basic_auth(self.username, Some(self.token.clone()))
+    }
+
+    fn generate_pulls_list(&self, repo: &Repository<'_>, page_no: u32) -> reqwest::RequestBuilder {
+        let pull_url = repo.pulls_url.replace("{/number}", "");
+        Client::new()
+            .get(&pull_url)
+            .header("Accept", "application/vnd.github.v3+json")
+            .header("User-Agent", self.username)
+            .query(&[
+                ("sort", "updated"),
+                ("direction", "desc"),
+                ("state", "open"),
+                ("per_page", "100"),
+                ("page", page_no.to_string().as_str()),
+            ])
+            .basic_auth(self.username, Some(self.token.clone()))
+    }
+
+    fn generate_merge_pr(&self, repo: &Repository<'_>, pull: &Pull<'_>) -> reqwest::RequestBuilder {
+        let merge_url = format!("{}/{}/merge", repo.pulls_url, pull.number);
+
+        Client::new()
+            .put(&merge_url)
+            .header("Accept", "application/vnd.github.v3+json")
+            .header("User-Agent", self.username)
+            .basic_auth(self.username, Some(self.token.clone()))
+    }
+}
+
+async fn merge_pr(
+    repo: &Repository<'_>,
+    pull: &Pull<'_>,
+    gh_handler: &GithubHandler<'_>,
+) -> Result<(), reqwest::Error> {
+    gh_handler.generate_merge_pr(repo, pull).send().await?;
 
     Ok(())
 }
 
 async fn detect_dependabot_prs(
     repo: &Repository<'_>,
-    username: &str,
+    gh_handler: &GithubHandler<'_>,
 ) -> Result<(), reqwest::Error> {
     println!(
         "{}",
@@ -42,22 +86,11 @@ async fn detect_dependabot_prs(
             .paint(format!("Detecting dependabot PRs for {}", repo.name))
     );
 
-    let pull_url = repo.pulls_url.replace("{/number}", "");
     let mut page_no: u32 = 1;
     let depdenda_re: Regex = Regex::new(r"^Bump.*from.*to.*$").unwrap();
     loop {
-        let response_text = Client::new()
-            .get(pull_url.as_str())
-            .header("Accept", "application/vnd.github.v3+json")
-            .header("User-Agent", username)
-            .query(&[
-                ("sort", "updated"),
-                ("direction", "desc"),
-                ("state", "open"),
-                ("per_page", "100"),
-                ("page", page_no.to_string().as_str()),
-            ])
-            .basic_auth(username, Some(CLONE_TOKEN.clone()))
+        let response_text = gh_handler
+            .generate_pulls_list(repo, page_no)
             .send()
             .await?
             .text()
@@ -74,7 +107,7 @@ async fn detect_dependabot_prs(
                 "{}",
                 Colour::Red.paint(format!("Performing merge of {}", pull.title))
             );
-            merge_pr(&pull_url, pull.number, username).await?;
+            merge_pr(&repo, &pull, &gh_handler).await?;
         }
 
         page_no += 1;
@@ -83,29 +116,18 @@ async fn detect_dependabot_prs(
     Ok(())
 }
 
-async fn iterate_repos(username: &str) -> Result<(), reqwest::Error> {
+async fn iterate_repos(gh_handler: &GithubHandler<'_>) -> Result<(), reqwest::Error> {
     println!(
         "\n{}\n",
         Colour::Blue.paint("Fetching repositories for the user")
     );
 
-    let repo_url = format!("https://api.github.com/users/{}/repos", username);
-
     let mut page_no: u32 = 1;
     // TODO: Convert this to iterator and chain it to the
     // pull request iterator in the second place.
     loop {
-        let response_text = Client::new()
-            .get(repo_url.as_str())
-            .header("Accept", "application/vnd.github.v3+json")
-            .header("User-Agent", username)
-            .query(&[
-                ("sort", "updated"),
-                ("direction", "desc"),
-                ("per_page", "100"),
-                ("page", page_no.to_string().as_str()),
-            ])
-            .basic_auth(username, Some(CLONE_TOKEN.clone()))
+        let response_text = gh_handler
+            .generate_repo_list(page_no)
             .send()
             .await?
             .text()
@@ -121,7 +143,7 @@ async fn iterate_repos(username: &str) -> Result<(), reqwest::Error> {
         page_no += 1;
 
         for repo in repo_list.iter() {
-            detect_dependabot_prs(repo, username).await?;
+            detect_dependabot_prs(repo, &gh_handler).await?;
         }
     }
 
@@ -134,6 +156,11 @@ async fn main() {
         .nth(1)
         .expect("Github username needs to be passed");
 
+    let gh_handler: GithubHandler = GithubHandler {
+        username: username.as_str(),
+        token: CLONE_TOKEN,
+    };
+
     println!(
         "{}",
         Colour::Cyan
@@ -141,5 +168,5 @@ async fn main() {
             .paint("Clean your dependabot PRs in fastest way")
     );
 
-    iterate_repos(&username).await.unwrap()
+    iterate_repos(&gh_handler).await.unwrap()
 }
